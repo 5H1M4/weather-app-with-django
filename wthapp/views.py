f0e5f55efebf7b46f07897abadcfa264e497645b
Endi Shima <endy.shima@gmail.com>
2025-01-13 14:16:37 +0100

    Changed the 5 day forecast bug and improved it

--- wthapp/views.py
+++ wthapp/views.py
@@ -14,17 +14,45 @@ def index(request):
         data = requests.get(url, params=PARAMS).json()
 
         if 'list' in data:
-            forecast = []
-            for forecast_entry in data['list']:
-                forecast.append({
-                    'date': forecast_entry['dt_txt'],
-                    'temp': forecast_entry['main']['temp'],
-                    'description': forecast_entry['weather'][0]['description'],
-                    'icon': forecast_entry['weather'][0]['icon']
-                })
+            # Dictionary to store forecasts by date
+            forecasts_by_date = {}
             
-            # Sort the forecast by date (and time)
-            forecast.sort(key=lambda x: x['date'])
+            for forecast_entry in data['list']:
+                # Convert timestamp to datetime
+                date_time = datetime.datetime.strptime(forecast_entry['dt_txt'], '%Y-%m-%d %H:%M:%S')
+                date = date_time.date()
+                hour = date_time.hour
+
+                # Categorize time periods
+                if 5 <= hour <= 11:
+                    time_period = 'morning'
+                elif 12 <= hour <= 17:
+                    time_period = 'midday'
+                elif 18 <= hour <= 23:
+                    time_period = 'night'
+                else:
+                    continue  # Skip other hours
+
+                # Store only one forecast per time period per day
+                if date not in forecasts_by_date:
+                    forecasts_by_date[date] = {}
+                
+                if time_period not in forecasts_by_date[date]:
+                    forecasts_by_date[date][time_period] = {
+                        'date': date,
+                        'time_period': time_period,
+                        'temp': forecast_entry['main']['temp'],
+                        'description': forecast_entry['weather'][0]['description'],
+                        'icon': forecast_entry['weather'][0]['icon']
+                    }
+
+            # Convert dictionary to list and sort by date
+            forecast = []
+            for date in sorted(forecasts_by_date.keys())[:5]:  # Limit to 5 days
+                day_forecasts = forecasts_by_date[date]
+                for period in ['morning', 'midday', 'night']:
+                    if period in day_forecasts:
+                        forecast.append(day_forecasts[period])
 
             context = {
                 'forecast': forecast,
